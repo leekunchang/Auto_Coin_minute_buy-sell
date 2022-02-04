0e8c592699a3ec1b9eea76fcfd57765220361db6
KUNCHANG LEE <lkc9233@naver.com>
2022-02-04 17:31:05 +0900

    minute초기모델

---
+++ Backtest_minute.py
@@ -0,0 +1,54 @@
+import pyupbit
+import numpy as np
+
+# OHLCV(open, high, low, close, volume)로 당일 시가, 고가, 저가, 종가, 거래량에 대한 데이터
+df = pyupbit.get_ohlcv("KRW-REP","minute1",3600)
+
+# 이동평균선 5분 기준 추가(window=고려일수)
+df['ma5'] = df['close'].rolling(window=5).mean()
+
+# 이동평균선 5분 기준 추가(window=고려일수)
+df['ma10'] = df['close'].rolling(window=10).mean()
+
+# 이동평균선 5분 기준 추가(window=고려일수)
+df['ma20'] = df['close'].rolling(window=20).mean()
+
+# 이동평균선 5분 기준 추가(window=고려일수)
+df['ma60'] = df['close'].rolling(window=60).mean()
+
+# 이동평균선 5분 기준 추가(window=고려일수)
+df['ma120'] = df['close'].rolling(window=120).mean()
+
+# # 변동폭 * k 계산, (고가 - 저가) * k값 - 삭제처리
+# df['range'] = (df['high'] - df['low']) * 0.5
+
+# # target(매수가), range 컬럼을 한칸씩 밑으로 내림(.shift(1))
+# df['target'] = df['open'] + df['range'].shift(1)
+
+# # bull = 전일종가 > 이평선보다 높을경우 TRUE
+# df['bull'] = df['open'] > df['ma5']
+
+# # bull = 이평선20 * 가중치 - 횡보장 거래 정지선
+df['bull'] = df['ma20'] * 1.05
+
+# ror(수익률), np.where(조건문, 참일때 값, 거짓일때 값)
+fee = 0.0005
+df['ror'] = np.where((df['ma5'] > df['ma10']) & (df['ma5'] > df['bull']), # 이평선 이상일 경우 값 추출 추가 MA버전 삽입분
+                      df['close'] / df['close'].shift(1) - fee,
+                      1)
+
+# 누적 곱 계산(cumprod) => 누적 수익률
+df['hpr'] = df['ror'].cumprod()
+
+# Draw Down 계산 (누적 최대 값과 현재 hpr 차이 / 누적 최대값 * 100)
+df['dd'] = (df['hpr'].cummax() - df['hpr']) / df['hpr'].cummax() * 100
+
+#MDD 계산
+print("MDD: ", df['dd'].max())
+
+#HPR 계산 (기간수익율)
+print("HPR: ", df['hpr'][-2])
+
+#엑셀로 출력
+df.to_excel("minute_ma.xlsx")
+print("OK")
--- Cointrade_minute_buy_sell.py
+++ Cointrade_minute_buy_sell.py
@@ -5,30 +5,23 @@ import datetime
 access = "K1izlIYmgptIBMaMhfaZlWh8KlFnUXOxIXmS91pA"
 secret = "x4vnFWp8mViKuunhEZwkAaojIomtTNnzVx6xMIDi"
 
-K_code = 0.5 # K 상수값
-B_code = 1.0205 # 익절 희망 % - 수수료포함 2프로 먹고 타겟보다 1% 높을때 매수. (최소마진 1%)
-coin_code = "WEMIX" # 종목코드
+coin_code = "JST" # 종목코드
 
 
-#수정전
+def get_ma5(ticker): # 분봉 22분 조회, 5분 이평선
+    df = pyupbit.get_ohlcv(ticker, interval="minute1", count=22)
+    ma5 = df['close'].rolling(5).mean()
+    return ma5
 
-def get_target_price(ticker, k):
-    """변동성 돌파 전략으로 매수 목표가 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
-    target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
-    return target_price
+def get_ma10(ticker): # 분봉 22, 조회 10분 이평선
+    df = pyupbit.get_ohlcv(ticker, interval="minute1", count=22)
+    ma10 = df['close'].rolling(10).mean()
+    return ma10
 
-def get_start_time(ticker):
-    """시작 시간 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
-    start_time = df.index[0]
-    return start_time
-
-def get_ma3(ticker): # MA버전 추가분
-    """3일 이동 평균선 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=3)
-    ma3 = df['close'].rolling(3).mean().iloc[-1]
-    return ma3
+def get_ma20(ticker): # 분봉 22, 조회 20분 이평선
+    df = pyupbit.get_ohlcv(ticker, interval="minute1", count=22)
+    ma20 = df['close'].rolling(20).mean()
+    return ma20
 
 def get_balance(ticker):
     """잔고 조회"""
@@ -41,10 +34,6 @@ def get_balance(ticker):
                 return 0
     return 0
 
-def get_current_price(ticker):
-    """현재가 조회"""
-    return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
-
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
@@ -52,25 +41,15 @@ print("autotrade start")
 # 자동매매 시작
 while True:
     try:
-        now = datetime.datetime.now()
-        start_time = get_start_time("KRW-"+coin_code)
-        end_time = start_time + datetime.timedelta(days=1)
-
-        if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-"+coin_code, K_code) # K상수값 K_code
-            ma3 = get_ma3("KRW-"+coin_code) # MA버전 삽입분
-            current_price = get_current_price("KRW-"+coin_code)
-            benefit_price = target_price * B_code # 익절희망값(변수로지정됨)
-            # 매수조건 - 목표가 and 이평선 < 현재가격 < 타겟가격 * 1.01 < 익절값
-            # 실제 매수조건은 목표+이평선 도달 시 매수, 그리고 매수가보다 1% 높으면 안사짐. 조건에 합할 경우 수차례 사짐
-            if target_price < current_price and ma3 < current_price and current_price < benefit_price and current_price < target_price * 1.01:
-                krw = get_balance("KRW")
-                if krw > 5000:
-                    upbit.buy_market_order("KRW-"+coin_code, krw*0.9995)
-            elif target_price < current_price and benefit_price < current_price: # 익절코드
-                coin_volume = get_balance(coin_code)
-                if coin_volume > 0.00008:
-                    upbit.sell_market_order("KRW-"+coin_code, coin_volume*0.9995)
+        ma5 = get_ma5("KRW-"+coin_code) # ma5 값 차트 불러오는 함수
+        ma10 = get_ma10("KRW-"+coin_code) # ma10 값 차트 불러오는 함수
+        ma20 = get_ma20("KRW-"+coin_code) # ma20 값 차트 불러오는 함수
+        # 매수조건 - 목표가 and 이평선 < 현재가격 < 타겟가격 * 1.01 < 익절값
+        # 실제 매수조건은 목표+이평선 도달 시 매수, 그리고 매수가보다 1% 높으면 안사짐. 조건에 합할 경우 수차례 사짐
+        if ma10 < ma5 and ma20 * 1.05 < ma5:
+            krw = get_balance("KRW")
+            if krw > 5000:
+                upbit.buy_market_order("KRW-"+coin_code, krw*0.9995)
         else:
             coin_volume = get_balance(coin_code)
             if coin_volume > 0.00008:
