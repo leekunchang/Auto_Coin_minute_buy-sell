a25d051d5b25a296c5a0056cf378669af1401832
KUNCHANG LEE <lkc9233@naver.com>
2022-03-02 09:05:05 +0900

    볼린저 업, 기본 + 리드미

--- Cointrade_bollinger_up1.py
+++ Cointrade_bollinger_up1.py
@@ -9,35 +9,35 @@ secret = "x4vnFWp8mViKuunhEZwkAaojIomtTNnzVx6xMIDi"
 coin_code = "OMG" 
 
 
-def get_ma20(ticker): # 60분봉 12분 조회, 5분 이평선
+def get_ma20(ticker): 
     df = pyupbit.get_ohlcv(ticker, interval="minute60", count=23)
-    ma20 = df['close'].rolling(20).mean().iloc[-1] # 현재 20시간 이평선 조회(-1)
+    ma20 = df['close'].rolling(20).mean().iloc[-1] 
     return ma20
 
-def get_ubb(ticker): # 60분봉 12분 조회, 5분 이평선
+def get_ubb(ticker): 
     df = pyupbit.get_ohlcv(ticker, interval="minute60", count=23)
-    df['ma20'] = df['close'].rolling(20).mean() # 현재 20시간 이평선 조회(-1)
+    df['ma20'] = df['close'].rolling(20).mean() 
     ubb = df['ma20'] + 2 * df['close'].rolling(window=20).std().iloc[-1]
     return ubb
 
-def get_ubb_c(ticker): # 60분봉 12분 조회, 5분 이평선
+def get_ubb_c(ticker): 
     df = pyupbit.get_ohlcv(ticker, interval="minute60", count=23)
-    df['ma20'] = df['close'].rolling(20).mean() # 현재 20시간 이평선 조회(-1)
+    df['ma20'] = df['close'].rolling(20).mean() 
     df['ubb'] = df['ma20'] + 2 * df['close'].rolling(window=20).std().iloc[-1]
     ubb_c = (df['ma20']+df['ubb']).iloc[-1] / 2
     return ubb_c
 
-def get_benefit(ticker): # 60분봉 12분 조회, 5분 이평선
+def get_benefit(ticker): 
     df = pyupbit.get_ohlcv(ticker, interval="minute60", count=23)
-    df['ma20'] = df['close'].rolling(20).mean() # 현재 20시간 이평선 조회(-1)
+    df['ma20'] = df['close'].rolling(20).mean() 
     df['ubb'] = df['ma20'] + 2 * df['close'].rolling(window=20).std().iloc[-1]
     df['ubb_c'] = (df['ma20']+df['ubb']) / 2
     ubb_benefit = (df['ubb_c']+df['ubb']).iloc[-1] / 2
     return ubb_benefit
 
-def get_exit(ticker): # 60분봉 12분 조회, 5분 이평선
+def get_exit(ticker):
     df = pyupbit.get_ohlcv(ticker, interval="minute60", count=23)
-    df['ma20'] = df['close'].rolling(20).mean() # 현재 20시간 이평선 조회(-1)
+    df['ma20'] = df['close'].rolling(20).mean() 
     df['ubb'] = df['ma20'] + 2 * df['close'].rolling(window=20).std().iloc[-1]
     df['ubb_c'] = (df['ma20']+df['ubb']) / 2
     ubb_exit = (df['ubb_c']+df['ma20']).iloc[-1] / 2
@@ -59,10 +59,6 @@ def get_current_price(ticker):
     """현재가 조회"""
     return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
 
-# 현재가 조회 보류상태
-# def get_current_price(ticker):
-#     """현재가 조회"""
-#     return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
 
 
 # 로그인
@@ -73,35 +69,35 @@ print("autotrade start")
 # 자동매매 시작
 while True:
     try:
-        ma20 = get_ma20("KRW-"+coin_code) # ma5 값 차트 불러오는 함수
-        ma_ubb = get_ubb("KRW-"+coin_code) # 1시간전 ma5 차트 불러오는 함수
-        ma_ubb_c = get_ubb_c("KRW-"+coin_code) # 2시간전 ma5 차트 불러오는 함수
-        ma_benefit = get_benefit("KRW-"+coin_code) # ma10 값 차트 불러오는 함수
+        ma20 = get_ma20("KRW-"+coin_code) 
+        ma_ubb = get_ubb("KRW-"+coin_code) 
+        ma_ubb_c = get_ubb_c("KRW-"+coin_code) 
+        ma_benefit = get_benefit("KRW-"+coin_code) 
         ma_exit = get_exit("KRW-"+coin_code)
         current_price = get_current_price("KRW-"+coin_code)
 
-        if ma_exit * 0.999 < current_price < ma_exit * 1.001:
+        if ma_ubb_c * 0.999 < current_price < ma_ubb_c * 1.001:
             krw = get_balance("KRW")
             if krw > 5000:
                 upbit.buy_market_order("KRW-"+coin_code, krw*0.9995)
                 print("매수")
-                # time.sleep(1800) # 매수 후 30분간 거래정지 (차트 등락에따른 불필요한 거래로 수수료손실 예방)
                 
-        if ma_exit * 1.015 < current_price:
+                
+        if ma_ubb_c * 1.015 < current_price:
             coin_volume = get_balance(coin_code)
             if coin_volume > 0.00008:
                 upbit.sell_market_order("KRW-"+coin_code, coin_volume*0.9995)
                 print("익절")
-                # time.sleep(1800) # 매수 후 30분간 거래정지 (차트 등락에따른 불필요한 거래로 수수료손실 예방)
+                
 
-        if current_price < ma20:
+        if current_price < ma_exit:
             coin_volume = get_balance(coin_code)
             if coin_volume > 0.00008:
                 upbit.sell_market_order("KRW-"+coin_code, coin_volume*0.9995)
                 print("손절")
-                # time.sleep(1800) # 매수 후 30분간 거래정지 (차트 등락에따른 불필요한 거래로 수수료손실 예방)        
+                     
 
-        if current_price < ma20 * 0.99:
+        if current_price < ma20:
             print("종료")
             break                
                 
